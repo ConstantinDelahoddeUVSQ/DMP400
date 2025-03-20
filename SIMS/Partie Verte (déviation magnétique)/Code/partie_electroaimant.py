04ad4c1b6a588c69684a58221099257071c9656e
Lucas Schaeffer <lucas.schaeffer@ens.uvsq.fr>
2025-03-20 12:20:33 +0100

    masses atomiques, charges en eV et rajout detecteur à partie electroaimant

--- SIMS/Partie Verte (déviation magnétique)/Code/partie_electroaimant.py
+++ SIMS/Partie Verte (déviation magnétique)/Code/partie_electroaimant.py
@@ -3,23 +3,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import fsolve 
+import scipy.constants as constants
 
 
 # Objet d'une particule décrite avec son rapport masse/charge et sa vitesse initiale
 class particule :
-    def __init__(self, rapport_masse_charge : float, v_initiale : float) :
+    def __init__(self, masse_charge : tuple[int, int], v_initiale : float) -> None :
         """
         Objet particule traversant un champ magnétique d'axe z
 
         Parameters
         ----------
-        rapport_masse_charge : float
-            Masse (en Kg) / Charge (en C) de la particule
+        masse_charge : tuple of int
+            Masse (en uintés atomiques), Charge (en eV) de la particule
         v_initiale : float
             Vitesse initiale en y de la particule (en m/s)   
         """
-        self.mq = rapport_masse_charge
+        self.mq = masse_charge[0] * constants.u / masse_charge[1] / constants.eV
         self.vo = v_initiale
+        self.m = masse_charge[0]
+        self.c = masse_charge[1]
 
 
     # Niveau 5 : L'equation de la trajectoire d'une particule en fonction de son rapport masse/charge et sa vitesse initiale
@@ -90,7 +93,7 @@ class particule :
 
         """
         x, y = self.trajectoire(Bz, x_min, x_max, n_points)
-        ax.plot(x, y, label=str(self.mq))
+        ax.plot(x, y, label=f'{self.m}u, {self.c}eV')
     
 
     # Niveau 2.1 : Détermine la puissance du champ magnétique nécéssaire pour dévier une particule à un point précis
@@ -119,28 +122,31 @@ class particule :
 
 
 # Niveau 2.2 : Tracer l'ensemble des trajectoires des particules d'un faisceau
-def tracer_ensemble_trajectoires(rapports_masse_charge_particules : list[float], vitesse_initiale : float, Bz : float, x_min : float, x_max : float) -> None:
+def tracer_ensemble_trajectoires(masses_charges_particules : list[tuple[int, int]], vitesse_initiale : float, Bz : float, x_detecteur : float) -> None:
     """
     Trace les trajectoires entre x_min et x_max pour un ensemble de particules d'un faisceau
 
     Parameters
     ----------
-    rapports_masse_charge_particules : list of float
-        Masse (en Kg) / Charge (en C)  pour toutes les particules
+    masses_charges_particules : list of tuple of int
+        Masse (en unités atomiques), Charge (en eV)  pour toutes les particules
     vitesse_initiale : float
         Vitesse intiale en y commune à toutes les particules du faisceau
     Bz : float
             Valeur du champ magnétique d'axe z (en T)
-    x_min : float
-            Position en x minimale (en m)
-        x_max : float
-            Position en x maximale (en m)
+    x_detecteur : float
+        L'abscisse du détecteur (en m)
     """
-    particules = [particule(rapport_masse_charge, vitesse_initiale) for rapport_masse_charge in rapports_masse_charge_particules]    # Liste d'objets particule représentant toutes les particules
+    particules = [particule(masse_charge, vitesse_initiale) for masse_charge in masses_charges_particules]    # Liste d'objets particule représentant toutes les particules
     fig, ax = plt.subplots()
-    for particule_locale in particules :
-        particule_locale.tracer_trajectoire(ax, Bz, x_min, x_max)
     
+    all_y_contact = []
+    for particule_locale in particules :
+        particule_locale.tracer_trajectoire(ax, Bz, 0, x_detecteur)
+        all_y_contact.append(particule_locale.equation_trajectoire(x_detecteur, Bz))
+
+    ax.plot([x_detecteur, x_detecteur], [min(all_y_contact) * 0.8, max(all_y_contact) * 1.1], c='black', linewidth=5, label='Détecteur')
+
     ax.legend()
     plt.show()
 
@@ -151,13 +157,13 @@ Test de la fonction tracer_ensemble_trajectoires (valeurs non représentatives)
 
 On trace les trajectoires de particules avec des rapports m/q différents dans un champ magnétique donné
 '''
-# if __name__ == '__main__' :
-#     rapports_masse_charge = [1e-27/1.602e-19, 2e-27/1.602e-19, 3e-27/1.602e-19]
-#     vitesse_initiale = 1
-#     Bz = 1.25e-08
-#     x_min, x_max = 0, 3
+if __name__ == '__main__' :
+    rapports_masse_charge = [(1, 1), (2, 1), (3, 1)]
+    vitesse_initiale = 1
+    Bz = 1.25e-08
+    x_detecteur = 0.2
     
-#     tracer_ensemble_trajectoires(rapports_masse_charge, vitesse_initiale, Bz, x_min, x_max)
+    tracer_ensemble_trajectoires(rapports_masse_charge, vitesse_initiale, Bz, x_detecteur)
 
 
 '''
